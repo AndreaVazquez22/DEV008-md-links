ced183b1cfc4313f97dbc0e27217d2cc4f565d48
AndreaVazquez22 <kar.andre@icloud.com>
2023-09-08 12:21:21 -0600

    Validacion

--- functions.js
+++ functions.js
@@ -1,35 +1,46 @@
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 
 const fileExist = (filepath) => {
   return fs.existsSync(filepath);
 };
 
+//Saber si la ruta es absoluta
 const pathAbsolute = (filepath) => {
   return path.isAbsolute(filepath);
 };
-const covertPath = (filepath) => {
+
+//Convierte ruta relativa en absoluta
+const convertPath = (filepath) => {
   return path.resolve(filepath);
 };
+
+//Funcion para sacar extensión
 const pathExt = (filepath) => {
   return path.extname(filepath);
 };
 
+//Saber si es un archivo
 const pathFile = (filepath) => {
   return fs.statSync(filepath).isFile();
 };
 
+//Saber si es un directivo
 const directoryPath = (filepath) => {
   return fs.statSync(filepath).isDirectory();
 };
 
+//Lee el archivo md
 const readFileMd = (filepath, callback) => {
   return fs.readFile(filepath, "utf-8", callback);
 };
 
-const readLinks = (textData) => {
+//Lee los links que hacen match con la expresión regular creando un array (allLinks)
+const readLinks = (textData, filepath) => {
   const fullLinkOnlyRegex = /\[[^\[\]]*\]\((http|https):\/\/[^\(\)]+\)/g;
   const allLinks = textData.match(fullLinkOnlyRegex);
+  //Recorre array allLinks y con método exec busca coincidencia sobre singleMatch
   const singleMatch = /\[([^\[]+)\]\((.*)\)/;
   const result = [];
 
@@ -40,17 +51,39 @@ const readLinks = (textData) => {
         completeLink: text[0],
         textLink: text[1],
         url: text[2],
+        file: filepath,
       });
     }
   }
+  return result;
+};
 
-  let output = [];
-  result.forEach((link) => {
-    output += "Texto: " + link.textLink + "\n";
-    output += "URL: " + link.url + "\n\n";
+const validateLinks = (links) => {
+    /*map es un método que itera sobre un arreglo y aplica la función a cada elemento del arreglo
+    para crear un nuevo arreglo con los nuevos elementos obtenidos*/
+const linkPromise = links.map((link) => {
+     //fetch metodo para solicitud HTTP GET por defecto a cada URL especifica 
+     // RESPONSE objeto que contiene info de la respuesta del servidor//
+    return fetch(link.url).then((response) => {
+      return {
+        text: link.textLink,
+        href: link.url,
+        status: response.status,
+        message: response.statusText,
+        file: link.file,
+      };
+    })  .catch(error => {
+        // Captura y maneja errores de conexión u otros errores
+        return {
+          text: link.textLink,
+          href: link.url,
+          status: error.status, // Puedes establecer un estado personalizado en caso de error
+          message: error.message,
+          file: link.file,
+        };
+      });
   });
-
-  return output;
+  return Promise.all(linkPromise);
 };
 
 module.exports = {
@@ -59,7 +92,8 @@ module.exports = {
   pathExt,
   pathFile,
   directoryPath,
-  covertPath,
+  convertPath,
   readFileMd,
   readLinks,
+  validateLinks,
 };
--- index.js
+++ index.js
@@ -1,47 +1,84 @@
 const functions = require("./functions.js");
 
 const mdLinks = () => {
-  const filepath = "C:\\Users\\Lenovo ThinkPad E580\\Documents\\Laboratoria\\DEV008-md-links\\README.md";
+  const filepath = "./README.md";
+  
+  //El archivo existe 
   const isFileExist = functions.fileExist(filepath);
   if (isFileExist) {
     console.log("El archivo existe");
 
+    //Mi ruta es relavita o absoluta
     const isPathAbsolute = functions.pathAbsolute(filepath);
     let finalPath = filepath;
     if (isPathAbsolute) {
       console.log("La ruta es Absoluta");
+
     } else {
-      const routeAbsolut = functions.covertPath(filepath);
+ //Convierte ruta relativa en absoluta
+      const routeAbsolut = functions.convertPath(filepath);
       console.log("La ruta NO es absoluta");
-      console.log(functions.covertPath(filepath));
+      console.log(functions.convertPath(filepath));
       finalPath = routeAbsolut;
     }
-
+//Es un archivo o no 
     const pathIsFile = functions.pathFile(finalPath);
     if (pathIsFile) {
       console.log("Es un archivo");
     } else {
       console.log("NO es un archivo");
     }
-
+//Identifica la extension md 
     const isExtMd = functions.pathExt(finalPath);
     if (isExtMd === ".md") {
       console.log("El archivo es MD");
-
+//Lee el archivo md 
       functions.readFileMd(finalPath, (err, data) => {
         if (err) {
           console.log("error: ", err);
+// Recorre archivo md y extrae links con su texto y url
         } else {
-          const getLinks = functions.readLinks(data);
-          console.log(getLinks);
+          const getLinks = functions.readLinks(data, finalPath);
+          //console.log(getLinks);
+          let validLinks = [];
+          let invalidLinks = [];
+          const linksValidate = functions.validateLinks(getLinks);
+          linksValidate.then(results =>{
+              results.forEach(result =>{
+                
+               
+                let infoAdditional = `
+                text: ${result.text},
+                href: ${result.href},
+                status: ${result.status},
+                message: ${result.message},
+                file: ${result.file},
+              `;
+                if(result.status === 200){
+                console.log(`El enlace "${result.text}" es válido. Estado: ${result.status}-${result.message}\n ${infoAdditional}`);
+                validLinks.push(result);
+                }else {
+                  console.log(`El enlace "${result.text}" no es válido. Estado: ${result.status} - ${result.message}\n ${infoAdditional}`);
+                invalidLinks.push(result);
+                }
+              });
+              console.log("Enlaces validos:", validLinks);
+              console.log("Enlaces invalidos:", invalidLinks);
+            }).catch(error => {
+      console.error("Error al validar los enlaces:", error);
+    });
+         
           
         }
+       
       });
     } else {
       console.log("El archivo NO es MD");
     }
+
   } else {
     console.log("El archivo NO existe");
   }
 };
 mdLinks();
+
